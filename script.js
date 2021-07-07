96ff1f32ea759b1d0510b914504f7848b2fff0c2
Sherry <sherry.t@live.ca>
2021-07-07 00:02:23 -0400

    Wrote code for javascript for pop up modules, created divs and styled them.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// PSEUDO CODE HERE.
+// ABOUT LINK POP UP MODAL 
+
+// Get the modal with text
+const popModal = document.getElementById('popup-modal');
+
+// Get the link that opens the modal
+const modalLink = document.getElementbyId('modal-link');
+
+// Get the element that closes the modal
+const closeModal = document.getElementsByClassName('close-modal')[0];
+
+// When user clicks on "About" link in the navigation, modal will pop up.  
+modalLink.addEventListener('click', function(){
+    popModal.style.display = "block";
+    console.log("modal pop!");
+});
+
+// When user clicks on the "X" icon, the modal will close. 
+closeModal.addEventListener('click', function(){
+    popModal.style.display = "none";
+});
+
