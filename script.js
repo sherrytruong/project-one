f7b21365dca58a34d4d1b766a821c72764a37279
Sherry <sherry.t@live.ca>
2021-07-07 01:25:09 -0400

    Finalized About pop-up modal css, html, and js.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // ABOUT LINK POP UP MODAL 
+// Inspired by: https://www.w3schools.com/howto/howto_css_modals.asp
 
 // Get the modal with text
 const popModal = document.getElementById('popup-modal');
 
 // Get the link that opens the modal
-const modalLink = document.getElementbyId('modal-link');
+const modalLink = document.getElementById('modal-link');
 
 // Get the element that closes the modal
 const closeModal = document.getElementsByClassName('close-modal')[0];
@@ -12,7 +13,6 @@ const closeModal = document.getElementsByClassName('close-modal')[0];
 // When user clicks on "About" link in the navigation, modal will pop up.  
 modalLink.addEventListener('click', function(){
     popModal.style.display = "block";
-    console.log("modal pop!");
 });
 
 // When user clicks on the "X" icon, the modal will close. 
